2582b21cdc104f360bacbec87e3553e6b5820e6b
dieum512 <akonkwadieum222@gmail.com>
2023-05-26 11:45:11 +0200

    Update the querries.sql file

--- queries.sql
+++ queries.sql
@@ -8,3 +8,33 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+/* Query and update animals table */
+
+BEGIN;
+SAVEPOINT SP1;
+ALTER TABLE animals RENAME COLUMN species TO unspecified;
+ROLLBACK TO SP1;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokeon' WHERE name NOT LIKE '%mon';
+COMMIT;
+
+BEGIN;
+SAVEPOINT SP2;
+DELETE FROM animals;
+ROLLBACK TO SP2;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg = -5.7;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg = -45;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg = -12.13;
+COMMIT;
+
+SAVEPOINT SP3;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP3;
+
+SELECT COUNT(name) FROM animals;
+SELECT * FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
