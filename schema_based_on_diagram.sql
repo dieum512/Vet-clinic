361c2f198d582cb68a9ea1c1c6f8d99f133bde73
dieum512 <akonkwadieum222@gmail.com>
2023-05-30 14:25:37 +0200

    Add medical table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE clinic;
  -- create table patients 
- CREATE TABLE patients( id INTEGER PRIMARY KEY, name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, ); 
+ CREATE TABLE patients( id INTEGER PRIMARY KEY, name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, );
+-- create table medical_histories 
+CREATE TABLE medical_histories( id INTEGER PRIMARY KEY, admitted_at TIMESTAMP, patient_id INTEGER REFERENCES patients(id), status VARCHAR(255), ); 
