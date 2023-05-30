67afa50c334534447fd4b0bc90812a2e369c1c10
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-05-30 17:02:01 +0430

    treatement table added to the project

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE clinic;
  -- create table patients 
- CREATE TABLE patients( id INTEGER PRIMARY KEY, name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, );
+ CREATE TABLE patients(id INTEGER PRIMARY KEY, name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL);
 -- create table medical_histories 
-CREATE TABLE medical_histories( id INTEGER PRIMARY KEY, admitted_at TIMESTAMP, patient_id INTEGER REFERENCES patients(id), status VARCHAR(255), );
+CREATE TABLE medical_histories(id INTEGER PRIMARY KEY, admitted_at TIMESTAMP, patient_id INTEGER REFERENCES patients(id), status VARCHAR(255));
 -- create table invoices 
-CREATE TABLE invoices( id INTEGER PRIMARY KEY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INTEGER REFERENCES medical_histories(id), ); 
+CREATE TABLE invoices(id INTEGER PRIMARY KEY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INTEGER REFERENCES medical_histories(id)); 
+--create table treatments
+CREATE TABLE treatments(id INTEGER  medical_histories(id), type VARCHAR(255), name VARCHAR(255));
