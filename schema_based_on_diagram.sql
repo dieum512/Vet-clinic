3e0240d7f3a1ba2a52e8f3cb2ba13b0f5ca40177
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-05-30 17:03:19 +0430

    invoice_items table added to the porject

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,3 +7,6 @@ CREATE TABLE medical_histories(id INTEGER PRIMARY KEY, admitted_at TIMESTAMP, pa
 CREATE TABLE invoices(id INTEGER PRIMARY KEY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INTEGER REFERENCES medical_histories(id)); 
 --create table treatments
 CREATE TABLE treatments(id INTEGER  medical_histories(id), type VARCHAR(255), name VARCHAR(255));
+
+-- create table invoice_items
+CREATE TABLE invoice_items(id INTEGER PRIMARY KEY, unit_price DECIMAL, quantity INTEGER, total_price DECIMAL, invoice_id INTEGER, treatment_id INTEGER REFERENCES treatments(id));
