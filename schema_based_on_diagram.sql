bd01d8f0b4170222f27cf4a959de7a207e5a8a63
dieum512 <akonkwadieum222@gmail.com>
2023-05-30 14:27:02 +0200

    Create invoice table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,4 +2,6 @@ CREATE DATABASE clinic;
  -- create table patients 
  CREATE TABLE patients( id INTEGER PRIMARY KEY, name VARCHAR(255) NOT NULL, date_of_birth DATE NOT NULL, );
 -- create table medical_histories 
-CREATE TABLE medical_histories( id INTEGER PRIMARY KEY, admitted_at TIMESTAMP, patient_id INTEGER REFERENCES patients(id), status VARCHAR(255), ); 
+CREATE TABLE medical_histories( id INTEGER PRIMARY KEY, admitted_at TIMESTAMP, patient_id INTEGER REFERENCES patients(id), status VARCHAR(255), );
+-- create table invoices 
+CREATE TABLE invoices( id INTEGER PRIMARY KEY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INTEGER REFERENCES medical_histories(id), ); 
